a377d85a3a5d14717d5f77fc2c495f495be96d99
baconandegg <pahulank@gmail.com>
2013-11-07 19:19:27 +0900

    ...

--- CPR/src/main/webapp/resources/js/nextJoin.js
+++ CPR/src/main/webapp/resources/js/nextJoin.js
@@ -174,7 +174,9 @@ $('#agreeBtn_se').on('click', function(){
 		      alert("에러 발생!!");
 		      return false;
 		 }else{
-		   
+//			var chk1 = $('#member_PW').val();
+//			var chk2 = $('#Sel_Member_PW_chk').val();
+
 			alert(data + "가입 완료!!");
 					
 			var $join02 = $('#join_seller');
@@ -187,12 +189,86 @@ $('#agreeBtn_se').on('click', function(){
 			$join03.css("display", "");
 			$st03.attr("class", "on");
 			
-			
 			// 지도 닫기를 위한거???
 			$('#map').attr('style', 'display:none');
 		 }
 	};	
 	$('#join_seller_form').ajaxForm(banana).submit();
+	
+//	$('#join_seller_form').ajaxForm({
+//		
+//		beforeSubmit: function(data){ 
+//			var chk1 = $('#Member_PW').val();
+//			var chk2 = $('#Member_PW_chk').val();
+//
+//			// 비밀번호 비교를 합니다 이푸...
+//			if (chk1.trim() == '') {
+//				alert("비번 null...");
+//			} else if (chk2.trim() == '') {
+//				alert("비번확인 null...");
+//			} else {
+//				if (chk1 == chk2) {
+//					
+//					$('strong').remove();	
+//	
+//					//여기서 file 제외하고 나머지 form만 컨트롤러 post로 보내고 받아 예외처리 해도 될듯.. - 즉 컨트롤러를 하나 만들어야 됨
+//	
+//					for ( var i = 0; i < data.length; i++) {
+//							
+//							if(data[i].field == "memberTel"){
+//								var tmpMsg = data[i].defaultMessage;
+//								console.log("memberTel : " + tmpMsg);
+//								$('#Member_TEL').after("<strong>" + tmpMsg + "</strong>");
+//							}
+//							
+//							if (data[i].field == "memberId") {
+//								var tmpMsg = data[i].defaultMessage;
+//								console.log("memberId : " + tmpMsg);
+//								$('#Member_ID').after("<strong>" + tmpMsg + "</strong>");
+//							}
+//
+//							if (data[i].field == "memberName") {
+//								var tmpMsg = data[i].defaultMessage;
+//								console.log("memberName : " + tmpMsg);
+//								$('#Member_NAME').after("<strong>" + tmpMsg + "</strong>");
+//							}
+//							
+//							$('strong').css("color","red");
+//							
+//						}
+//						$('strong').fadeToggle(2000, "linear");									
+//				}else {
+//					alert("비번과 비번확인 두개 다름");
+//				}
+//			}
+//		},
+//		success: function(state, data){
+//			console.log("data : " + data);
+//			console.log("state : " + state);
+//			
+//			if (data=="error"){
+//			      alert("에러 발생!!");
+//			      return false;
+//			 }else{
+//				
+//				alert(data + "가입 완료!!");
+//						
+//				var $join02 = $('#join_seller');
+//				var $join03 = $('#join03');
+//				var $st02 = $('#st02');
+//				var $st03 = $('#st03');
+//		
+//				$join02.css("display", "none");
+//				$st02.attr("class", "");
+//				$join03.css("display", "");
+//				$st03.attr("class", "on");
+//				
+//				
+//				// 지도 닫기를 위한거???
+//				$('#map').attr('style', 'display:none');
+//			 }
+//		}
+//	}).submit();
 	
 });
 
